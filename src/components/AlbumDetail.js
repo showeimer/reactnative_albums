481429cf6839b0dfab1b08a71d60e0959de061b4
showeimer <showeimer@gmail.com>
2017-09-07 20:58:47 -0400

    added album image to card

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -5,28 +5,31 @@ import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
 
-  const { title, artist, thumbnail_image } = album;
+  const { title, artist, thumbnail_image, image } = album;
   const {
     headerContentStyle,
     headerTextStyle,
     thumbnailStyle,
-    thumbnailContainerStyle } = styles;
+    thumbnailContainerStyle,
+    imageStyle } = styles;
 
   return (
     <Card>
       <CardSection>
-        <View style={thumbnailContainerStyle}>
+        <View style={ thumbnailContainerStyle }>
           <Image
-            style={thumbnailStyle}
-            source={{ uri: thumbnail_image}}
+            style={ thumbnailStyle }
+            source={{ uri: thumbnail_image }}
           />
         </View>
-        <View style={headerContentStyle}>
-          <Text style={headerTextStyle}>{title}</Text>
-          <Text>{artist}</Text>
+        <View style={ headerContentStyle }>
+          <Text style={ headerTextStyle }>{ title }</Text>
+          <Text>{ artist }</Text>
         </View>
       </CardSection>
-
+      <CardSection>
+        <Image style={ imageStyle } source={{ uri: image }} />
+      </CardSection>
     </Card>
   );
 };
@@ -48,6 +51,11 @@ const styles = {
     alignItems: 'center',
     marginLeft: 10,
     marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 }
 
